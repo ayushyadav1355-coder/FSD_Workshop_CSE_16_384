15ce848324a653e4a23d2c43f3b04dea522bc941
Ayush Yadav <ayushyadav1355@gmail.com>
2026-09-01 11:45:29 +0530

    change commit

--- Backend/LAB3/httpServer.js
+++ Backend/LAB3/httpServer.js
@@ -1,27 +1,35 @@
 import http from "http";
-const userdata =[{
-    id:1,
-    name:"Ayush",
-    email:"ayushyadav1355@gmail.com"
-}]
+
+const userdata = [
+  {
+    id: 1,
+    name: "Ayush",
+    email: "ayushyadav1355@gmail.com",
+  },
+];
+
 const server = http.createServer((req, res) => {
-    // res.end("hello server");
-    const url =req.url;
-    const method = req.method
-    if(url == "/msg" && method == 'GET'){
-        res.end("this is welcome message from server");
+  const url = req.url;
+  const method = req.method;
+
+  if (url == "/msg" && method == "GET") {
+    res.end("this is welcome message from server");
+  } else if (url == "/sys" && method == "GET") {
+    res.end("this is system information");
+  } else if (url == "/data" && method == "GET") {
+    res.statusCode = 201;
+    res.end(JSON.stringify(userdata));
+  } else if (url.startsWith("/users/") && method == "GET") {
+    const id = url.split("/")[2];
+    console.log(id);
+    const user = userdata.find((u) => u.id == id);
+    if (!user) {
+      return res.end("user not found");
     }
-    else if (url == "/sys" && method =="GET")
-    {
-        res.end("this is system information");
-    }
-    else if(url == "/data" && method == "GET")
-    {
-        res.statusCode =201;
-        res.end(JSON.stringify(userdata));
-    }
+    res.end(JSON.stringify(user));
+  }
 });
 
 server.listen(2000, () => {
-    console.log("server is running on port number 3000");
-})
+  console.log("server is running on port number 3000");
+});
